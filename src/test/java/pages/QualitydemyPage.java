870e2f044d686ada65f4776405d0e3010ef6b65d
erhan <erhanpaker@gmail.com>
2023-03-24 13:50:17 +0300

    first commit

--- src/test/java/pages/QualitydemyPage.java
+++ src/test/java/pages/QualitydemyPage.java
@@ -1,2 +1,29 @@
-package pages;public class QualitydemyPage {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class QualitydemyPage {
+
+    public QualitydemyPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[text()='Accept']")
+    public WebElement cookies;
+
+    @FindBy(xpath = "//a[@class='btn btn-sign-in-simple']")
+    public WebElement login;
+
+    @FindBy(xpath = "//input[@type='email']")
+    public WebElement emailButonu;
+
+    @FindBy(xpath = "//input[@type='password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "(//button[@type='submit'])[2]")
+    public WebElement loginButton;
+
 }
--- src/test/java/tests/QualitydemyTest.java
+++ src/test/java/tests/QualitydemyTest.java
@@ -1,2 +1,31 @@
-package tests;public class QualitydemyTest {
+package tests;
+
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class QualitydemyTest {
+
+    QualitydemyPage quality = new QualitydemyPage();
+
+    @Test
+
+    public void qualitydemyTest() throws InterruptedException {
+
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+
+        quality.cookies.click();
+
+        quality.login.click();
+
+        quality.emailButonu.sendKeys(ConfigReader.getProperty("GecerliMail"));
+
+        Thread.sleep(500);
+
+        quality.passwordBox.sendKeys(ConfigReader.getProperty("GecerliPassword"));
+
+        quality.loginButton.click();
+
+    }
 }
